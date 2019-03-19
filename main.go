99168488ab8ea5a737e034563dc1a338954d28fe
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-03-19 15:38:51 +0100

    Put db response into struct

--- main.go
+++ main.go
@@ -4,9 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
+
+type weather struct {
+	Place         string
+	LowTemp       int
+	HighTemp      int
+	Precipitation int
+	Date          time.Time
+}
+
+type weatherReports struct {
+	WeatherReports []weather
+}
 
 func main() {
 	connStr := "dbname=pqgotest sslmode=disable"
@@ -18,11 +31,33 @@ func main() {
 	defer db.Close()
 	fmt.Println("Successfully connected to pqgotest")
 
-	var tempLow int
-	err = db.QueryRow("SELECT temp_lo FROM weather WHERE place = 'Snøhetta'").Scan(&tempLow)
+	rows, err := db.Query(`
+		SELECT *
+		FROM weather`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("The temperature at Snøhetta is %d\n", tempLow)
+	defer rows.Close()
+
+	weatherReports := weatherReports{}
+	for rows.Next() {
+		report := weather{}
+		err = rows.Scan(
+			&report.Place,
+			&report.LowTemp,
+			&report.HighTemp,
+			&report.Precipitation,
+			&report.Date,
+		)
+		weatherReports.WeatherReports = append(weatherReports.WeatherReports, report)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, report := range weatherReports.WeatherReports {
+		fmt.Printf("The weather data for %s was read from the db\n", report.Place)
+	}
 }
